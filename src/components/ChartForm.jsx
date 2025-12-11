122f30400f5304ab1bb0e88c495db726700f60a9
akashs-sudo <akashs@clovity.com>
2025-12-11 21:11:53 +0530

    create dynamic tab

--- src/components/ChartForm.jsx
+++ src/components/ChartForm.jsx
@@ -2,7 +2,7 @@ import { FiCheckCircle } from "react-icons/fi";
 
 export default function ChartForm() {
   return (
-    <div className="w-full bg-white p-4">
+    <div className="w-full bg-white px-4">
       {/* GRID WRAPPER */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
         {/* Reference ID */}
@@ -13,7 +13,7 @@ export default function ChartForm() {
           <input
             type="text"
             placeholder="D47B65"
-            className="border border-gray-300 rounded-md px-3 py-2 text-sm focus:ring-2 focus:ring-blue-400 focus:outline-none"
+            className="border border-gray-300 rounded-xs px-3 py-2 text-sm focus:ring-2 focus:ring-blue-400 focus:outline-none placeholder-gray-900"
           />
         </div>
 
@@ -25,7 +25,7 @@ export default function ChartForm() {
           <input
             type="text"
             placeholder="Your chart title here..."
-            className="border border-gray-300 rounded-md px-3 py-2 text-sm w-full focus:ring-2 focus:ring-blue-400 focus:outline-none"
+            className="border border-gray-300 rounded-xs px-3 py-2 text-sm w-full focus:ring-2 focus:ring-blue-400 focus:outline-none placeholder-gray-900"
           />
         </div>
 
@@ -34,7 +34,7 @@ export default function ChartForm() {
           <label className="text-sm font-semibold text-gray-700 mb-1">
             Title Alignment
           </label>
-          <select className="border border-gray-300 rounded-md px-3 py-2 text-sm focus:ring-2 focus:ring-blue-400 focus:outline-none">
+          <select className="border border-gray-300 rounded-xs px-3 py-2 text-sm focus:ring-2 focus:ring-blue-400 focus:outline-none placeholder-gray-900">
             <option>Left</option>
             <option>Center</option>
             <option>Right</option>
@@ -46,7 +46,7 @@ export default function ChartForm() {
           <label className="text-sm font-semibold text-gray-700 mb-1">
             Source
           </label>
-          <select className="border border-gray-300 rounded-md px-3 py-2 text-sm focus:ring-2 focus:ring-blue-400 focus:outline-none">
+          <select className="border border-gray-300 rounded-xs px-3 py-2 text-sm focus:ring-2 focus:ring-blue-400 focus:outline-none placeholder-gray-900">
             <option>Projects</option>
             <option>Issues</option>
             <option>Sprints</option>
@@ -60,7 +60,7 @@ export default function ChartForm() {
           </label>
 
           <div className="flex items-center gap-2 w-full">
-            <select className="border border-gray-300 rounded-md px-3 py-2 text-sm w-full focus:ring-2 focus:ring-blue-400 focus:outline-none">
+            <select className="border border-gray-300 rounded-xs px-3 py-2 text-sm w-full focus:ring-2 focus:ring-blue-400 focus:outline-none">
               <option>App (APP), AppVantage (AP)</option>
               <option>Backend</option>
               <option>Frontend</option>
